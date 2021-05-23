c2b26351f52e6d56d578cac8068f73e0ea0d0a70
jakemperry <Jakemperry@gmail.com>
2021-05-23 12:31:55 -0700

    update to upload for Music_ML3 table on DynamoDB

--- Jake/uploadToDynamoTable.py
+++ Jake/uploadToDynamoTable.py
@@ -4,7 +4,7 @@ import json
 #this script is to help you load a fairly simple JSON file to DynamoDB
 dynamo_client= boto3.resource('dynamodb', region_name='us-west-1')  #establish connection to your dynamoDB
 
-table = dynamo_client.Table('Music_ML') #assign YOUR tablename here
+table = dynamo_client.Table('Music_ML3') #assign YOUR tablename here
 file="../Resources/MLResources/full_Imploded_pt1_jmp.json" #full path to your json data
 
 #this will loop through your file and put_item each row into your table
@@ -16,12 +16,15 @@ with open(file) as json_file:
         name = song['name']
         artists = song['artists']
         genres = song['genres']
-        assignments = int(song['Assignments'])
+        assignments = song['Assignments']
 
         print("Adding Song:", name)
 
         table.put_item(
            Item={
-               'name':name
+               'name':name,
+               'artists':artists,
+               'genres':genres,
+               'Assignments':assignments
             }
         )
